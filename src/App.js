75f43a2825031fa1b78d3d51cfde286059508cde
djemeli Tsapi Josias <djemelitsapijosias@gmail.com>
2025-09-10 18:30:48 +0100

    login form

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import LoginPage from "./components/LoginPage";
 
 function App() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-  fetch("http://localhost:5000/api/db-test")
-    .then(res => res.json())
-    .then(data => setMessage("DB Time: " + JSON.stringify(data.time)))
-    .catch(err => setMessage("Error: " + err));
-}, []);
-
-  return (
-    <div>
-      <h1>Frontend is running 🎉</h1>
-      <p>Backend says: {message}</p>
-    </div>
-  );
+  return <LoginPage />;
 }
 
 export default App;
